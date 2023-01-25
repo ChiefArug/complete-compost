1bd8a1101bb9f0857cfa7a13cb876440a9cb8edd
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-01-26 12:38:57 +1300

    Nonnull all the things!

---
+++ src/main/java/chiefarug/mods/complete_compost/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package chiefarug.mods.complete_compost;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
