4929a9b9a0c90e2961490d9d3cbb90de217d1920
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-01-26 16:13:54 +1300

    Fix up Component stuff to use 1.19.2 methods

--- src/main/java/chiefarug/mods/complete_compost/compat/CompleteCompostJEIPlugin.java
+++ src/main/java/chiefarug/mods/complete_compost/compat/CompleteCompostJEIPlugin.java
@@ -5,12 +5,11 @@ import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.RegistryObject;
+
 
 import static chiefarug.mods.complete_compost.CompleteCompost.MODID;
 import static chiefarug.mods.complete_compost.Registry.*;
@@ -18,7 +17,7 @@ import static chiefarug.mods.complete_compost.Registry.*;
 @JeiPlugin
 public class CompleteCompostJEIPlugin implements IModPlugin {
 
-	private static final TextComponent newLine = new TextComponent("\n");
+	private static final Component newLine = Component.literal("\n");
 
 	@Override
 	public ResourceLocation getPluginUid() {
@@ -28,29 +27,29 @@ public class CompleteCompostJEIPlugin implements IModPlugin {
 	@Override
 	public void registerRecipes(IRecipeRegistration reg) {
 		addIngredientInfo(reg, COMPOST_BLOCK_ITEM,
-				new TranslatableComponent("description.complete_compost.generic_compost"),
+				Component.translatable("description.complete_compost.generic_compost"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.waterloggable_compost"),
+				Component.translatable("description.complete_compost.waterloggable_compost"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.compost.limitations"),
+				Component.translatable("description.complete_compost.compost.limitations"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.compost.crafting")
+				Component.translatable("description.complete_compost.compost.crafting")
 		);
 		addIngredientInfo(reg, MYSTICAL_COMPOST_BLOCK_ITEM,
-				new TranslatableComponent("description.complete_compost.generic_compost"),
+				Component.translatable("description.complete_compost.generic_compost"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.waterloggable_compost"),
+				Component.translatable("description.complete_compost.waterloggable_compost"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.mystical_compost.limitations")
+				Component.translatable("description.complete_compost.mystical_compost.limitations")
 		);
 		addIngredientInfo(reg, LUMINANT_COMPOST_BLOCK_ITEM,
-				new TranslatableComponent("description.complete_compost.generic_compost"),
+				Component.translatable("description.complete_compost.generic_compost"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.waterloggable_compost"),
+				Component.translatable("description.complete_compost.waterloggable_compost"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.compost.limitations"),
+				Component.translatable("description.complete_compost.compost.limitations"),
 				newLine,
-				new TranslatableComponent("description.complete_compost.luminant_compost.it_glows")
+				Component.translatable("description.complete_compost.luminant_compost.it_glows")
 		);
 	}
 
